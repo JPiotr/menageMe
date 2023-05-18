0c541978b50d7e305a69bbf33583aa8fbabebfa3
Oblitus <piotr5jurczak@gmail.com>
2023-05-18 15:39:13 +0200

    add basic header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ProjectsListComponent } from './components/projects-list/projects-list.component';
 import { ProjectComponent } from './components/project/project.component';
 import {HttpClientModule} from "@angular/common/http";
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectsListComponent,
-    ProjectComponent
+    ProjectComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
